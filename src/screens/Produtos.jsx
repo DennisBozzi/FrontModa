4fc46355739c14a879caa2553aa7db66462ba921
Dennis Bozzi <dennisbozzii@gmail.com>
2024-07-04 21:12:04 -0300

    Ajustes

--- src/screens/Produtos.jsx
+++ src/screens/Produtos.jsx
@@ -1,34 +1,130 @@
 import React from "react";
 import { MenuNavigation } from "./MenuNavigation";
 import { Button } from "@/components/ui/button"
-import { PlusCircle } from "lucide-react"
+import { PlusCircle, MoreHorizontal } from "lucide-react"
+import { Badge } from "@/components/ui/badge"
 import {
   Tabs,
   TabsContent,
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export function Produtos() {
   return <>
     <MenuNavigation>
       <main className=" items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-        <div className="flex place-content-between">
-          <Tabs defaultValue="todos">
+        <Tabs defaultValue="todos">
+          <div className="flex place-content-between">
             <TabsList>
               <TabsTrigger value="todos">Todos</TabsTrigger>
               <TabsTrigger value="ativos">Ativos</TabsTrigger>
               <TabsTrigger value="vendidos">Vendidos</TabsTrigger>
             </TabsList>
-          </Tabs>
-          <Button size="sm" className="h-7 gap-1">
-            <PlusCircle className="h-3.5 w-3.5" />
-            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-              Novo Produto
-            </span>
-          </Button>
-        </div>
+
+            <Button size="sm" className="h-7 gap-1">
+              <PlusCircle className="h-3.5 w-3.5" />
+              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                Novo Produto
+              </span>
+            </Button>
+          </div>
+
+
+          <TabsContent value="todos">
+            <Card x-chunk="dashboard-06-chunk-0">
+              <CardHeader>
+                <CardTitle>Produtos</CardTitle>
+                <CardDescription>
+                  Gerencia os preços e as informações dos produtos.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>Nome</TableHead>
+                      <TableHead>Status</TableHead>
+                      <TableHead>Preço</TableHead>
+                      <TableHead>
+                        <span className="sr-only">Actions</span>
+                      </TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                  <TableRow>
+                      <TableCell className="font-medium"> Bermuda </TableCell>
+                      <TableCell>
+                        <Badge variant="outline">Vendido</Badge>
+                      </TableCell>
+                      <TableCell>15</TableCell>
+                      <TableCell>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button aria-haspopup="true" size="icon" variant="ghost">
+                              <MoreHorizontal className="h-4 w-4" />
+                              <span className="sr-only">Opções</span>
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+                            <DropdownMenuItem>Editar</DropdownMenuItem>
+                            <DropdownMenuItem>Excluir</DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className="font-medium"> Cueca </TableCell>
+                      <TableCell>
+                        <Badge variant="outline">Ativo</Badge>
+                      </TableCell>
+                      <TableCell>5</TableCell>
+                      <TableCell>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button aria-haspopup="true" size="icon" variant="ghost">
+                              <MoreHorizontal className="h-4 w-4" />
+                              <span className="sr-only">Opções</span>
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+                            <DropdownMenuItem>Editar</DropdownMenuItem>
+                            <DropdownMenuItem>Excluir</DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
       </main>
-    </MenuNavigation>
+    </MenuNavigation >
   </>
 }
